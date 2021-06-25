51cfb1339d270e5449a86042a1572bfc05179d9d
Daniel Nunes <danielzcomputacao@gmail.com>
2021-06-25 15:44:36 -0300

    Room page

--- letmeask/src/pages/NewRoom.tsx
+++ letmeask/src/pages/NewRoom.tsx
@@ -1,14 +1,38 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { FormEvent } from "react";
 
 import illustrationImg from "../assets/images/illustration.svg";
 import LogoImg from "../assets/images/logo.svg";
 
 import "../styles/auth.scss";
 import { Button } from "../components/Button";
-//import { useAuth } from "../hooks/useAuth";
+import { useState } from "react";
+
+import { database } from "../services/firebase";
+
+import { useAuth } from "../hooks/useAuth";
 
 export function NewRoom() {
-  //const { user } = useAuth();
+  const { user } = useAuth();
+  const history = useHistory();
+  const [newRoom, setNewRoom] = useState("");
+
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (newRoom.trim() === "") {
+      return;
+    }
+
+    const roomRef = database.ref("rooms");
+
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+    });
+
+    history.push(`/rooms/${firebaseRoom.key}`);
+  }
 
   return (
     <div id="page-auth">
@@ -24,8 +48,13 @@ export function NewRoom() {
         <div className="main-content">
           <img src={LogoImg} alt="Letmeask" />
           <h2>Criar uma nova Sala</h2>
-          <form>
-            <input type="text" placeholder="Nome da sala" />
+          <form onSubmit={handleCreateRoom}>
+            <input
+              type="text"
+              placeholder="Nome da sala"
+              onChange={(event) => setNewRoom(event.target.value)}
+              value={newRoom}
+            />
             <Button type="submit">Criar sala</Button>
           </form>
           <p>
---
+++ letmeask/src/pages/Room.tsx
@@ -0,0 +1,33 @@
+import "../styles/room.scss";
+
+import { Button } from "../components/Button";
+
+import logoimg from "../assets/images/logo.svg";
+
+export function Room() {
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoimg} alt="letmeask" />
+          <div>codigo</div>
+        </div>
+      </header>
+      <main className="content">
+        <div className="room-title">
+          <h1>Sala React</h1>
+          <span>4 perguntas</span>
+        </div>
+        <form>
+          <textarea placeholder="O que você quer perguntar?" />
+          <div className="form-footer">
+            <span>
+              Para enviar uma pergunta, <button>faça seu login </button>.
+            </span>
+            <Button type="submit">Enviar pergunta</Button>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
+}
